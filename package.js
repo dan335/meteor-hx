d612432b05d43c8cbe68519640f1a38b11dfaf89
Daniel Phillips <danphi@gmail.com>
2015-09-25 22:06:12 -0700

    meteor 1.2

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
 	name: "danimal:hx",
 	summary: "Library for hex grids.",
-	version: "1.0.8",
+	version: "1.0.9",
 	git: "https://github.com/dan335/meteor-hx.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR-CORE@0.9.0-rc12');
-  api.export('Hx', ['client', 'server'])
+  api.versionsFrom('1.2');
+	api.use(['underscore', 'check']);
+  api.export('Hx');
   api.addFiles('hx.js');
 });
